fd2d20305088ae634da844409be6b7f99a861f7d
kmrn <hello@kamranpayne.com>
2020-09-14 18:46:49 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# discord-record-info
+
+A Discord bot that helps you easily get more info or the cover art for and album in a Discord chat.
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+/**
+ * spincord
+ *
+ * Discord bot for linking discogs info in chat
+ */
+
+import Discord from 'discord.js';
+
+const client = new Discord.Client();
+
+client.on('message', (message) => {
+    if (message.author.bot) return;
+    console.log(message.content);
+});
+client.login(process.env.SPINCORD_BOT_TOKEN);
