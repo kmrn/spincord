acfade7a9e12eb33b26c3a912b1b61a0cdd09f3c
kmrn <hello@kamranpayne.com>
2020-09-15 00:49:54 -0400

    add bot command handling on message event

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,30 @@
  * Discord bot for linking discogs info in chat
  */
 
-import Discord from 'discord.js';
+import { Client, Message } from 'discord.js';
 
-const client = new Discord.Client();
+const prefix = '!';
+const client = new Client();
 
-client.on('message', (message) => {
-    if (message.author.bot) return;
-    console.log(message.content);
+client.on('ready', () => {
+    console.log('spincord is now ready for grailz');
+});
+
+client.on('message', async (message: Message) => {
+    const { content, author } = message;
+    if (author.bot || !content.startsWith(prefix)) {
+        return;
+    }
+    const [commandString] = content.split(' ');
+    const command = commandString.slice(prefix.length).toLowerCase();
+    const args = content.slice(commandString.length);
+
+    switch (command) {
+        case 'album':
+            break;
+    }
+
+    message.reply(`wow thats like my favorite album`);
 });
 
 client.login(process.env.SPINCORD_BOT_TOKEN);
