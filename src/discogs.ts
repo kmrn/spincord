82fc1180433922ec0c4db276bb7dd5a0ab9d8b8f
kmrn <hello@kamranpayne.com>
2020-09-16 19:01:24 -0400

    refactor auth headers, add release details getter

--- src/discogs.ts
+++ src/discogs.ts
@@ -1,13 +1,50 @@
 import { request } from 'https';
 import { escape } from 'querystring';
 
+const { SPINCORD_DISCOGS_KEY, SPINCORD_DISCOGS_SECRET } = process.env;
+const DISCOGS_HEADERS = {
+    Authorization: `Discogs key=${SPINCORD_DISCOGS_KEY}, secret=${SPINCORD_DISCOGS_SECRET}`,
+    'Content-Type': 'application/json',
+    'User-Agent': 'SpincordDiscordBot/1.0 +https://github.com/kmrn/spincord',
+};
+
 interface Result {
     country: string;
     year: string;
     title: string;
     cover_image: string;
     resource_url: string;
+    uri: string;
 }
+
+export const getReleaseDetails = (resourceUrl: string): Promise<void> => {
+    const url = new URL(resourceUrl);
+    const options = { headers: DISCOGS_HEADERS };
+    return new Promise((resolve, reject) => {
+        const responseData: Buffer[] = [];
+        const req = request(url, options, (res) => {
+            res.on('data', (chunk) => {
+                responseData.push(chunk);
+            });
+
+            res.on('end', () => {
+                const buffer = responseData.join('');
+                const listing = JSON.parse(buffer);
+                resolve(listing);
+            });
+
+            res.on('error', (error: Error) => {
+                reject(error);
+            });
+        });
+
+        req.on('error', (error: Error) => {
+            reject(error);
+        });
+
+        req.end();
+    });
+};
 
 export const findAlbum = async (album: string): Promise<Result> => {
     const results = await searchDiscogs(album);
@@ -17,17 +54,10 @@ export const findAlbum = async (album: string): Promise<Result> => {
 
 export const searchDiscogs = (query: string): Promise<Result[]> => {
     const escapedQuery = escape(query);
-    const key = process.env.SPINCORD_DISCOGS_KEY;
-    const secret = process.env.SPINCORD_DISCOGS_SECRET;
     const options = {
         hostname: 'api.discogs.com',
         path: `/database/search?q=${escapedQuery}&type=release&page=1&per_page=3`,
-        method: 'GET',
-        headers: {
-            Authorization: `Discogs key=${key}, secret=${secret}`,
-            'Content-Type': 'application/json',
-            'User-Agent': 'SpincordDiscordBot/1.0 +https://github.com/kmrn/spincord',
-        },
+        headers: DISCOGS_HEADERS,
     };
 
     return new Promise((resolve, reject) => {
