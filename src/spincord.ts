60ebb90018cfc4ff6655fbf12e35c95c95493751
kmrn <hello@kamranpayne.com>
2020-09-17 00:22:14 -0400

    add pricecheck command

--- src/spincord.ts
+++ src/spincord.ts
@@ -3,7 +3,25 @@
  */
 
 import { Message, MessageAttachment } from 'discord.js';
-import { findAlbum } from './discogs';
+import { findAlbum, getReleaseDetails } from './discogs';
+
+export const getStartingPrice = async (query: string): Promise<string> => {
+    const { resource_url } = await findAlbum(query);
+    const release = await getReleaseDetails(resource_url);
+    const { id, title, num_for_sale, lowest_price } = release;
+    const marketplaceUrl = `https://www.discogs.com/sell/release/${id.toString()}`;
+    const startingPrice = lowest_price.toFixed(2);
+    if (num_for_sale < 1) {
+        return `Sorry but it looks like you're the only one trying to sell one of those right now.\n\n${marketplaceUrl}`;
+    }
+    if (lowest_price > 500) {
+        return `WOAH! *Certified RARE grail.* There's currently ${num_for_sale} of those listed for sale starting at ***$${startingPrice}***!! :money_with_wings:\n\n${marketplaceUrl}`;
+    }
+    if (lowest_price > 100) {
+        return `Wow! Must be pretty rare. ${num_for_sale} of those are listed for sale right now starting at **$${startingPrice}**.\n\n${marketplaceUrl}`;
+    }
+    return `There are ${num_for_sale} listings for ${title} starting at $${startingPrice}.\n\n${marketplaceUrl}`;
+};
 
 export const getAlbumArt = async (query: string): Promise<MessageAttachment> => {
     const { cover_image } = await findAlbum(query);
@@ -33,7 +51,7 @@ export const Spincord = async (message: Message, command: string, query: string)
             channel.send(await getAlbumArt(query));
             break;
         case 'pricecheck':
-            message.reply('this ones not ready yet');
+            channel.send(await getStartingPrice(query));
             break;
         case 'spincord':
         case 'spincordhelp':
