3896ef9da1bd5304a4e02d9a95895f05976aadc0
kmrn <hello@kamranpayne.com>
2020-09-17 00:45:03 -0400

    add pricecheck unit test

--- __tests__/spincord.spec.ts
+++ __tests__/spincord.spec.ts
@@ -1,5 +1,5 @@
 import { MessageAttachment } from 'discord.js';
-import { getAlbumArt, getAlbumInfo } from '../src/spincord';
+import { getAlbumArt, getAlbumInfo, getStartingPrice } from '../src/spincord';
 
 test("gets the album cover for vulfpeck's the beautiful game", async () => {
     expect.assertions(1);
@@ -11,4 +11,10 @@ test("gets a URL for king gizzard's gumboot soup", async () => {
     expect.assertions(1);
     const url = await getAlbumInfo('gumboot soup');
     expect(url).toBe('https://discogs.com/King-Gizzard-And-The-Lizard-Wizard-Gumboot-Soup/release/11747418');
+});
+
+test("price check pink floyd's the dark side of the moon", async () => {
+    expect.assertions(2);
+    const response = await getStartingPrice('the dark side of the moon');
+    expect(response).toBeInstanceOf(String);
 });
