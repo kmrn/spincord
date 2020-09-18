9d0f523f2919e107c1e1c4a771d9f4edabf77f72
kmrn <hello@kamranpayne.com>
2020-09-18 11:17:22 -0400

    remove commented out code

--- __tests__/spincord.spec.ts
+++ __tests__/spincord.spec.ts
@@ -1,5 +1,5 @@
 import { MessageAttachment } from 'discord.js';
-import { getAlbumArt, getAlbumInfo, getStartingPrice } from '../src/spincord';
+import { getAlbumArt, getAlbumInfo } from '../src/spincord';
 
 test("gets the album cover for vulfpeck's the beautiful game", async () => {
     expect.assertions(1);
@@ -12,9 +12,3 @@ test("gets a URL for king gizzard's gumboot soup", async () => {
     const url = await getAlbumInfo('gumboot soup');
     expect(url).toBe('https://discogs.com/King-Gizzard-And-The-Lizard-Wizard-Gumboot-Soup/release/11747418');
 });
-
-// test("price check pink floyd's the dark side of the moon", async () => {
-//     expect.assertions(2);
-//     const response = await getStartingPrice('the dark side of the moon');
-//     expect(response).toBeTruthy();
-// });
