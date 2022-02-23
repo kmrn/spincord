9d381e7ab241938f611e6d2abef8eb1eac1b311a
dantco <danteamicolighting@gmail.com>
2022-02-22 22:36:35 -0500

    Add "slash commands" blurb #29

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,30 @@
  */
 
 import 'dotenv/config';
-import { Client, Intents } from 'discord.js';
+import { Client, Intents, Interaction, Message } from 'discord.js';
 import Spincord from './spincord/spincord';
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const spincord = new Spincord();
 
 client.on('ready', () => {
     console.log('Spincord is now listening for commands...');
 });
 
-client.on('interactionCreate', async (interaction) => {
+client.on('interactionCreate', async (interaction: Interaction) => {
     if (!interaction.isCommand()) return;
 
     try {
         await spincord.sendResponse(interaction);
     } catch (error) {
         interaction.reply(`Ran into some trouble with that one.\n\`\`\`${error}\`\`\``);
+    }
+});
+
+client.on('messageCreate', (message: Message) => {
+    const { content, author } = message;
+    if (!author.bot && content.startsWith('!spincord')) {
+        message.reply("Spincord now uses slash commands! Type /spincord to see what's up.");
     }
 });
 
